b39313171189f7a615f6d5188e5fd57946512f95
Angeal185 <beneaves01@hotmail.com>
2017-02-03 18:24:41 +0800

    Add files via upload

---
+++ application.py
@@ -0,0 +1,23 @@
+import info
+from flask import Flask
+from flask import render_template
+
+application = Flask(__name__)
+
+layout_args = {
+    "meta" : info.meta,
+    "navigation" : info.navigation,
+	"footer" : info.footerIcons,
+	"contact" : info.contact
+}
+
+@application.route("/")	
+@application.route("/index")
+@application.route("/index.jnj")
+def home():
+    return render_template("index.jnj", gallery=info.gallery, filter=info.filter, portfolio=info.portfolio, clogo=info.clogo, team=info.team, carousel=info.carousel, headings=info.headings, lorem=info.lorem, service=info.service, **layout_args)
+
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
---
+++ info.py
@@ -0,0 +1,319 @@
+meta = {
+    "title" : "website",
+    "author" : "ben eaves",
+    "description" : "add description here",
+    "url" : "",
+    "iconpath" : "",
+    "keywords" : "",
+	"staticCss" : "static/css/",
+	"staticJs" : "static/js/",
+	"staticImg" : "static/img/",
+	"year":"2016"
+	
+}
+
+navigation = [
+    {
+        "name" : "Home",
+        "link" : "#header",
+		"status" : "current",
+    },
+    {
+        "name" : "Services",
+        "link" : "#section-services",
+    },
+	{
+        "name" : "Reviews",
+        "link" : "#section-reviews",
+    },
+	{
+        "name" : "Team",
+        "link" : "#section-about",
+    },
+	{
+        "name" : "Portfolio",
+        "link" : "#section-works",
+    },
+    {
+        "name" : "Contact",
+        "link" : "#section-contact"
+    },
+	{
+        "name" : "location",
+        "link" : "#section-map",
+    }
+]
+
+service = [
+    {
+        "delay" : ".2s",
+        "icon" : "code",
+		"info" : "add info here",
+    },
+    {
+        "delay" : ".6s",
+        "icon" : "terminal",
+		"info" : "add info here",
+    },
+	{
+        "delay" : ".1s",
+        "icon" : "bolt",
+		"info" : "add info here",
+    }
+]
+
+gallery = [
+    {
+        "info" : "",
+		"img" : "static/img/demo.png",
+		"link":"#",
+		"name":"",
+		"email":"",
+		"slide":"1"
+    },
+    {
+        "info" : "",
+		"img" : "static/img/demo.png",
+		"link":"#",
+		"name":"",
+		"email":"",
+		"slide":"2"
+    },
+	{
+        "info" : "",
+		"img" : "static/img/demo.png",
+		"link":"#",
+		"name":"",
+		"email":"",
+		"slide":"3"
+    },
+	{
+        "info" : "",
+		"img" : "static/img/demo.png",
+		"link":"#",
+		"name":"",
+		"email":"",
+		"slide":"4"
+    }
+]
+
+clogo = [
+    {
+        "href" : "#!",
+        "img" : "cliogo.png",
+    },
+	    {
+        "href" : "#!",
+        "img" : "cliogo.png",
+    },
+	{
+        "href" : "#!",
+        "img" : "cliogo.png",
+    },
+	    {
+        "href" : "#",
+        "img" : "cliogo.png",
+    },
+	{
+        "href" : "#!",
+        "img" : "cliogo.png",
+    }
+]
+
+team = [
+    {
+		"delay" : ".2s",
+        "img" : "avatar.jpg",
+		"name" : "",
+        "title" : "",
+		"about" : "",
+    },
+	{
+		"delay" : ".4s",
+        "img" : "avatar.jpg",
+		"name" : "",
+        "title" : "",
+		"about" : "",
+    },
+	{
+		"delay" : ".6s",
+        "img" : "avatar.jpg",
+		"name" : "",
+        "title" : "",
+		"about" : "",
+    },
+	{
+		"delay" : ".8s",
+        "img" : "avatar.jpg",
+		"name" : "",
+        "title" : "",
+		"about" : "",
+    },
+]
+
+filter = [
+    {
+		"class" : "current",
+        "name" : "All",
+        "filter" : "*",
+    },
+	{
+        "name" : "Webdesign",
+        "filter" : ".webdesign",
+    },
+	{
+        "name" : "Photography",
+        "filter" : ".photography",
+    },
+	{
+        "name" : "Print",
+        "filter" : ".print",
+    },
+    
+]
+
+portfolio = [
+    {
+        "duration" : ".2s",
+		"img" : "portfolio/img1.jpg",
+		"category":"webdesign",
+		"name":"",
+    },
+	{
+        "duration" : ".4s",
+		"img" : "portfolio/img1.jpg",
+		"category":"photography",
+		"name":"",
+    },
+	{
+        "duration" : ".6s",
+		"img" : "portfolio/img1.jpg",
+		"category":"print",
+		"name":"",
+    },
+	{
+        "duration" : ".2s",
+		"img" : "portfolio/img1.jpg",
+		"category":"webdesign",
+		"name":"",
+    },
+	{
+        "duration" : ".4s",
+		"img" : "portfolio/img1.jpg",
+		"category":"photography",
+		"name":"",
+    },
+	{
+        "duration" : ".6s",
+		"img" : "portfolio/img1.jpg",
+		"category":"print",
+		"name":"",
+    },
+	{
+        "duration" : ".2s",
+		"img" : "portfolio/img1.jpg",
+		"category":"webdesign",
+		"name":"",
+    },
+	{
+        "duration" : ".4s",
+		"img" : "portfolio/img1.jpg",
+		"category":"photography",
+		"name":"",
+    },
+	{
+        "duration" : ".6s",
+		"img" : "portfolio/img1.jpg",
+		"category":"print",
+		"name":"",
+    }
+]
+
+carousel = [
+    {
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    },
+	{
+        "class" : "carousel-cell"
+    }
+]
+
+footerIcons = [
+    {
+		"href" : "#",
+        "class" : "icoRss",
+        "title" : "Rss",
+		"icon" : "rss",
+    },
+	{
+		"href" : "#",
+        "class" : "icoFacebook",
+        "title" : "Facebook",
+		"icon" : "facebook",
+    },
+	{
+		"href" : "#",
+        "class" : "icoTwitter",
+        "title" : "Twitter",
+		"icon" : "twitter",
+    },
+	{
+		"href" : "#",
+        "class" : "icoGoogle",
+        "title" : "Google +",
+		"icon" : "google-plus",
+    },
+	{
+		"href" : "#",
+        "class" : "icoLinkedin",
+        "title" : "Linkedin",
+		"icon" : "linkedin",
+    }
+]
+
+contact = {
+    "name" : "Your Name",
+    "email" : "Your Email",
+    "subject" : "Your Subject",
+    "message" : "Your Message",
+    "send" : "SEND MESSAGE"
+}
+
+
+headings = {
+    "about" : "who we are",
+    "review" : "Some of our reviews",
+    "team" : "our team members",
+    "portfolio" : "our portfolio",
+    "contact" : "contact us",
+    "location" : "our location"
+}
+
+lorem = {
+    "title" : "Add Title Here",
+    "name" : "Add Name Here",
+	"email" : "admin@admin.com",
+    "description" : "add description here",
+    "category" : "Add category Here",
+    "lorem" : "Integer nec odio",
+    "lorem2" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.",
+	"lorem3" : "Fapibus non interdum quis, suscipit nec dolor. Vivamus tempor tempus mauris vitae fermentum. In vitae nulla lacus. Sed sagittis tortor vel arcu sollicitudin nec tincidunt metus suscipit.Nunc velit risus, dapibus non interdum."	
+}
