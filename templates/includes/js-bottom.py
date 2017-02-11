afb5cb2decc4e4b2760b97852a1421402479bfab
Angeal185 <beneaves01@hotmail.com>
2017-02-11 20:03:56 +0800

    Update js-bottom.py

--- templates/includes/js-bottom.py
+++ templates/includes/js-bottom.py
@@ -1,9 +1,9 @@
 <script src="{{ meta.staticJs }}jquery.js"></script>
-<script src="{{ meta.staticJs }}bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.2/isotope.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.0.5/flickity.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js"></script>
-<script src="{{ meta.staticJs }}wow.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
 <script src="{{ meta.staticJs }}scroll.js"></script>
 <script src="{{ meta.staticJs }}gmap.js?sensor=false"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/classie/1.0.1/classie.min.js"></script>
