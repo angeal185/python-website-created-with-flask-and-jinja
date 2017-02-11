f59d5cf114ee0b8a53116a4fb53b916250e65bbe
Angeal185 <beneaves01@hotmail.com>
2017-02-11 19:54:42 +0800

    Update and rename js-bottom.jnj to js-bottom.py

--- templates/includes/js-bottom.py
+++ templates/includes/js-bottom.py
@@ -1,8 +1,7 @@
-﻿<script src="{{ meta.staticJs }}jquery.js"></script>
+<script src="{{ meta.staticJs }}jquery.js"></script>
 <script src="{{ meta.staticJs }}bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.2/isotope.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.0.5/flickity.pkgd.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js"></script>
 <script src="{{ meta.staticJs }}wow.js"></script>
 <script src="{{ meta.staticJs }}scroll.js"></script>
